850218fd2c22838709062d78f9c9d1c9fd3ccc78
Ranjan Verma <verma.ranjan62@gmail.com>
2026-03-16 13:24:41 +0530

    Product model created

---
+++ models/Product.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Product name is required"],
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: [true, "Product price is required"],
+      min: [0, "Price cannot be negative"],
+    },
+    description: {
+      type: String,
+      required: [true, "Product description is required"],
+    },
+    stockQuantity: {
+      type: Number,
+      required: [true, "Stock quantity is required"],
+      min: [0, "Stock cannot be negative"],
+      default: 0,
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("Product", productSchema);
